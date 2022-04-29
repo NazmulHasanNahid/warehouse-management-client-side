c1ef605ee31b1a721990d92f67f13ec504b72590
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-04-29 14:36:20 +0600

    project setup

---
+++ src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+     return (
+          <div className='text-center'>
+              <h1 className='text-danger fw-bold display-1'>404</h1> 
+              <p>This Page Doesn't Exist ..!</p>
+          </div>
+     );
+};
+
+export default NotFound;
