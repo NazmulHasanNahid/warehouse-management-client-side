abe9b85dc7bda13f48bd4cb436fa8f7816f19655
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-04-30 21:16:41 +0600

    products added section

--- src/components/pages/Inventory/Inventory.js
+++ src/components/pages/Inventory/Inventory.js
@@ -2,6 +2,9 @@ import { Button, Col, Row } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
 
 const Inventory = () => {
   const { id } = useParams({});
@@ -26,6 +29,22 @@ const Inventory = () => {
         })
     }
   }
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+       const url  = `https://limitless-springs-85910.herokuapp.com/products`
+       fetch(url ,{
+            method:'POST',
+            headers:{
+                 'content-type':"application/json"
+            },
+            body: JSON.stringify(data)
+       })
+       .then(res=> res.json())
+       .then(result =>{
+            toast('Product Added')
+            
+       })
+     };
   return (
     <div className="container my-5">
       <Row xs={1} md={2} className="g-5  container mx-auto ">
@@ -51,6 +70,15 @@ const Inventory = () => {
       </Col>
       <Col>
        <h3 className="text-center fw-bold">Add Products</h3>
+       <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+        <input className="mb-2" placeholder="Name" {...register("name", { required: true, maxLength: 20 })} />
+        <input className="mb-2" placeholder="Quantity" {...register("quantity", { required: true, maxLength: 20 })} />
+        <input className="mb-2" placeholder="supliarname" {...register("supliarname", { required: true, maxLength: 20 })} />
+        <textarea className="mb-2" placeholder="Descreption" {...register("description")} />
+        <input className="mb-2" placeholder="price" type="number" {...register("price")} />
+        <input className="mb-2" placeholder="Photo Url" type="text" {...register("img")} />
+        <input type="submit" value="Add" />
+      </form>
       </Col>
       </Row>
      
